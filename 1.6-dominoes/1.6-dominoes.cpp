a2956b098d741cf5eef744661043d07da734a532
AndreyM <222mushkin@gmail.com>
2022-03-09 22:41:49 +0300

    [1.6-dominoes] added flag 'excluded' for element, now using vector instead of list

--- 1.6-dominoes/1.6-dominoes.cpp
+++ 1.6-dominoes/1.6-dominoes.cpp
@@ -1,24 +1,28 @@
 #include <iostream>
 #include <fstream>
-#include <list>
+#include <vector>
 #include <string>
+#include <chrono>
 
 using namespace std;
 
-typedef pair<short, short> Domino;
-
-list<Domino> ReadDominoes(istream& in)
+struct Domino
 {
-	short n;
-	Domino d;
-	list<Domino> dl;
+	pair<short, short> val;
+	bool excluded;
+};
+
+vector<Domino> ReadDominoes(istream& in)
+{
+	short n, val1, val2;
+	vector<Domino> dl;
 
 	in >> n;
 
 	for (short i = 0; i < n; ++i)
 	{
-		in >> get<0>(d) >> get<1>(d);
-		dl.push_back(d);
+		in >> val1 >> val2;
+		dl.push_back({ pair<short, short>(val1, val2), false });
 	}
 
 	return dl;
@@ -26,48 +30,54 @@ list<Domino> ReadDominoes(istream& in)
 
 void AppendDominoToStr(string& str, Domino d)
 {
-	str.append(to_string(get<0>(d))).append(to_string(get<1>(d)));
+	str.append(to_string(get<0>(d.val))).append(to_string(get<1>(d.val)));
 }
 
-list<Domino> ExcludeDominoAtPos(list<Domino> dl, size_t pos)
+void ExcludeDominoAtPos(vector<Domino>& dl, size_t pos, bool exclude)
 {
-	auto it = dl.begin();
-	advance(it, pos);
-	dl.erase(it);
-
-	return dl;
+	dl[pos].excluded = exclude;
 }
 
 Domino SwapDomino(Domino d)
 {
-	return pair(get<1>(d), get<0>(d));
+	return { pair(get<1>(d.val), get<0>(d.val)), d.excluded };
 }
 
-string AddNextDomino(string str, Domino d, list<Domino> dl)
+string AddNextDomino(string str, Domino d, vector<Domino>& dl)
 {
 	AppendDominoToStr(str, d);
 
 	string nextStr, maxStr = str;
-	short nextDomino = get<1>(d);
+	short nextDomino = get<1>(d.val);
 	size_t i = 0;
 	for (Domino d : dl)
 	{
-		if (nextDomino == get<0>(d))
+		if (d.excluded) 
 		{
-			if ((nextStr = AddNextDomino(str, d, ExcludeDominoAtPos(dl, i))).compare(maxStr) > 0 || 
-				nextStr.length() > maxStr.length())
-			{
-				maxStr = nextStr;
-			}
+			++i;
+			continue;
 		}
 
-		if (nextDomino == get<1>(d))
+		if (nextDomino == get<0>(d.val))
 		{
-			if ((nextStr = AddNextDomino(str, SwapDomino(d), ExcludeDominoAtPos(dl, i))).compare(maxStr) > 0 || 
+			ExcludeDominoAtPos(dl, i, true);
+			if ((nextStr = AddNextDomino(str, d, dl)).compare(maxStr) > 0 || 
 				nextStr.length() > maxStr.length())
 			{
 				maxStr = nextStr;
 			}
+			ExcludeDominoAtPos(dl, i, false);
+		}
+
+		if (nextDomino == get<1>(d.val))
+		{
+			ExcludeDominoAtPos(dl, i, true);
+			if ((nextStr = AddNextDomino(str, SwapDomino(d), dl)).compare(maxStr) > 0 || 
+				nextStr.length() > maxStr.length())
+			{
+				maxStr = nextStr;
+			}
+			ExcludeDominoAtPos(dl, i, false);
 		}
 
 		++i;
@@ -76,29 +86,25 @@ string AddNextDomino(string str, Domino d, list<Domino> dl)
 	return maxStr;
 }
 
-string Start(list<Domino> dl)
+string Start(vector<Domino>& dl)
 {
 	string resStr, curStr;
 	size_t i = 0;
 	for (Domino d : dl)
 	{
-		curStr = AddNextDomino("", d, ExcludeDominoAtPos(dl, i));
+		ExcludeDominoAtPos(dl, i, true);
+		curStr = AddNextDomino("", d, dl);
 		if ((curStr.length() > resStr.length()) || ((curStr.length() == resStr.length()) && curStr.compare(resStr) > 0))
 		{
 			resStr = curStr;
-			cout << "changed\n";
 		}
 
-		cout << curStr << '\n';
-
-		curStr = AddNextDomino("", SwapDomino(d), ExcludeDominoAtPos(dl, i));
+		curStr = AddNextDomino("", SwapDomino(d), dl);
 		if ((curStr.length() > resStr.length()) || (curStr.length() == resStr.length() && curStr.compare(resStr) > 0))
 		{
 			resStr = curStr;
-			cout << "changed\n";
 		}
-
-		cout << curStr << '\n';
+		ExcludeDominoAtPos(dl, i, false);
 
 		++i;
 	}
@@ -108,12 +114,23 @@ string Start(list<Domino> dl)
 
 int main()
 {
+	auto start = std::chrono::steady_clock::now();
+
 	ifstream in("input.txt");
 	ofstream out("output.txt");
 
-	list<Domino> dominoes = ReadDominoes(in);
+	if (!in.is_open() || !out.is_open())
+	{
+		cout << "Failed to open files\n";
+		return 1;
+	}
+
+	vector<Domino> dominoes = ReadDominoes(in);
 
 	out << Start(dominoes) << '\n';
 
+	auto end = std::chrono::steady_clock::now();
+
+	cout << "Running time " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << " microseconds\n";
 	return 0;
 }
